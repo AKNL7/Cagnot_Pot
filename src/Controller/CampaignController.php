546c4973b4ad8d32edbff64f348e7228e5835d0e
AKNL <aknlsame@gmail.com>
2024-03-18 09:16:06 +0100

    commitBeforeDeploy

--- src/Controller/CampaignController.php
+++ src/Controller/CampaignController.php
@@ -28,12 +28,12 @@ class CampaignController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $campaign = new Campaign(); //Nouvel objet Campaign
-       
+
         $form = $this->createForm(CampaignType::class, $campaign); // Il va créer un formulaire a partir de la page CampaignType et va envoyer le formulaire avec la fonction render
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) { 
-           $campaign->setId();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $campaign->setId();
             $entityManager->persist($campaign);
             $entityManager->flush();
 
@@ -48,21 +48,46 @@ class CampaignController extends AbstractController
 
     #[Route('/{id}/show', name: 'app_campaign_show', methods: ['GET'])]
     // L'id ici envoye dans la route est utilise pour instancier l'objet Campaign qui correspond a l'id de la route dans la base de données. 
+
+
     public function show(Campaign $campaign, PaymentRepository $paymentRepository, ParticipantRepository $participantRepository): Response //l'objet campagne est recuperer ici
     {
         $allParticipantsFromCampaign = $participantRepository->findBy(['campaign' => $campaign]);
-        $allPaymentsFromCampaign = $paymentRepository->findBy();
+        // dd($allParticipantsFromCampaign);
+        $allPaymentsFromCampaign = $paymentRepository->findBy([]);
+
+        // va donner la somme des payment des participants 
+        $countParticipants = count($allParticipantsFromCampaign);
+        $totalCountAmount = 0; // On le met a zero pour ajouter les autres payments 
+
+        foreach ($allPaymentsFromCampaign as $payment) {
+            $totalCountAmount += $payment->getAmount(); // On boucle sur les payments et on ajoute ici les montants des amount
+        }
+
+        $progressBar = $totalCountAmount / $campaign->getGoal() * 100; // Le total des paiment divisé par l'objectif de la campagne multiplié par 100 pour avoir un pourcentage 
         
+        
+        $progressBar = floor($progressBar); // arrondi le nombre 
+      
+      
+      
+        // dd(floor($progressBar));
+        // dd($totalCountAmount);
+        // dd($campaign, $allParticipantsFromCampaign, $allPaymentsFromCampaign);
 
-
-        dd($campaign, $allParticipantsFromCampaign);
- 
         return $this->render('campaign/show.html.twig', [
             'campaign' => $campaign,
-            'participants' => $allParticipants
-            //La fonction show nous renvoie unu campaign 
+            'participants' => $allParticipantsFromCampaign,
+            'payments' => $allPaymentsFromCampaign,
+            'countParticipants' => $countParticipants,
+            'totalCountAmount' => $totalCountAmount,
+            'progressBar' => $progressBar
+
+            //La fonction show nous renvoie une campaign 
         ]);
     }
+
+
 
     #[Route('/{id}/edit', name: 'app_campaign_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Campaign $campaign, EntityManagerInterface $entityManager): Response
@@ -85,7 +110,7 @@ class CampaignController extends AbstractController
     #[Route('/{id}', name: 'app_campaign_delete', methods: ['POST'])]
     public function delete(Request $request, Campaign $campaign, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$campaign->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $campaign->getId(), $request->request->get('_token'))) {
             $entityManager->remove($campaign);
             $entityManager->flush();
         }
--- src/Form/CampaignType.php
+++ src/Form/CampaignType.php
@@ -3,20 +3,43 @@
 namespace App\Form;
 
 use App\Entity\Campaign;
+// use Doctrine\DBAL\Types\IntegerType;
+// use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+// use Symfony\Component\Form\FormTypeInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CampaignType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options ): void
     {
         $builder
-       
-            ->add('title')
-            ->add('content')
-            ->add('goal')
-            ->add('name')
+
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'validate'
+                ]
+            ])
+            ->add('content', TextType::class, [
+                'attr' => [
+                    'class' => 'materialize-textarea'
+                ]
+               
+            ])
+            ->add('goal', IntegerType::class, [
+                'attr' => [
+                    'class' => 'validate'
+                ]
+            ])
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => 'validate'
+                ]
+            ])
+          
             // ->add('created_at')
             // ->add('updated_at')
         ;
